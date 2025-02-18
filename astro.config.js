b593c3be406891ef939a08dff08fbfd04f0dcf23
Thomas Deinhamer <code@thasmo.dev>
2025-02-18 12:56:35 +0100

    Log debug information

--- astro.config.js
+++ astro.config.js
@@ -4,6 +4,10 @@ import { loadEnv } from 'vite';
 
 const { APPLICATION_SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
+console.log('env', process.env);
+console.log('mode', process.env.NODE_ENV);
+console.log('site', APPLICATION_SITE);
+
 export default defineConfig({
 	base: '/',
 	integrations: [sitemap()],
